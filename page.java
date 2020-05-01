e1ce12c80d2a49bdb0cad97c67fe9a73b3f51a22
Abirami091995 <saprinabi@gmail.com>
2020-05-01 11:18:04 -0700

    done

---
+++ page.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class page {
+	public static void main(String[] args) {
+		System.out.println("abi");
+	}
+
+}
---
+++ task2.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class task2 {
+	public static void main(String[] args) {
+		System.out.println("b");
+		System.out.println("c");
+	}
+
+}
